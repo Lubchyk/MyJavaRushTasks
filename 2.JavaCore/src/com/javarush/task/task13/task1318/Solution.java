bd292d7be0d19e5bd546d852de5eebd834133a29
Lubchyk <lubchyk@yandex.ua>
2017-02-20 23:36:21 +0200

    Add  tasks

--- 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -1,16 +1,35 @@
 package com.javarush.task.task13.task1318;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.io.FileInputStream;
 
 /* 
 Чтение файла
+1. Считать с консоли имя файла.
+2. Вывести в консоль(на экран) содержимое файла.
+3. Не забыть освободить ресурсы. Закрыть поток чтения с файла и поток ввода с клавиатуры.
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        // напишите тут ваш код
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sourceFileName = reader.readLine();
+        FileInputStream fileInputStream = new java.io.FileInputStream(sourceFileName);
+
+        int s = 0;
+        while (fileInputStream.available() > 0) {
+
+            s = fileInputStream.read();
+
+
+        }
+
+        System.out.print((char) s);
+        reader.close();
+        fileInputStream.close();
+//C:\Users\АНТ\Desktop\55.txt
     }
 }
