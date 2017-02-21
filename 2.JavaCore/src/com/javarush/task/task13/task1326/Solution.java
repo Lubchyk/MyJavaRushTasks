b5917a7a962ecb643a89b1cea0117abf7cac0c10
Lubchyk <lubchyk@yandex.ua>
2017-02-21 23:39:46 +0200

    Add  tasks

--- 2.JavaCore/src/com/javarush/task/task13/task1326/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1326/Solution.java
@@ -29,26 +29,19 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader reader1 = new BufferedReader(new InputStreamReader(new FileInputStream(reader.readLine())));
-        //FileReader fileReader = new FileReader(reader.readLine());
-        //FileInputStream fileInputStream = new FileInputStream(reader.readLine());
+        FileInputStream fileInputStream = new FileInputStream(reader.readLine());
+        BufferedReader reader1 = new BufferedReader(new InputStreamReader(fileInputStream));
         List<Integer> list = new ArrayList<>();
         String line;
         int data;
         while ((line = reader1.readLine()) != null) {
             data = Integer.parseInt(line);
             list.add(data);
-            //System.out.println(data);
         }
-        reader.close();
-        reader1.close();
-        //fileReader.close();
         int temp;
         for (int i = 0; i < list.size(); i++) {
-
-            for (int j = 0; j < list.size(); j++) {
-                if (list.size() < i + 1) break;
-                if (list.get(i) > list.get(i + 1)) {
+            for (int j = i; j < list.size(); j++) {
+                if (list.get(i) > list.get(j)) {
                     temp = list.get(i);
                     list.set(i, list.get(j));
                     list.set(j, temp);
@@ -56,11 +49,11 @@ public class Solution {
             }
         }
         for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 == 0)
             System.out.println(list.get(i));
         }
-
-
-
-    } //C:\Users\mlv\Desktop\55.txt
-
+        reader.close();
+        reader1.close();
+        fileInputStream.close();
+    } //C:\Users\mlv\Desktop\55.txt C:\Users\АНТ\Desktop\55.txt
 }
