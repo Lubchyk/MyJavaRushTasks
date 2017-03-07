e7044a32cf5bb07ff4274504c15cc04b697387bd
mlv <lubchyk@yandex.ua>
2017-03-07 16:59:57 +0200

    add new task

--- 2.JavaCore/src/com/javarush/task/task15/task1503/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1503/Solution.java
@@ -22,18 +22,29 @@ public class Solution {
         new Solution.Lanos().printlnDesire();
     }
 
-    public static class Ferrari {
+    public static class Ferrari extends LuxuriousCar {
         public void printlnDesire() {
-            //add your code here
+            System.out.println(Constants.WANT_STRING + Constants.FERRARI_NAME);
         }
     }
 
-    public static class Lanos {
+    public static class Lanos extends CheapCar {
         public void printlnDesire() {
-            //add your code here
+            System.out.println(Constants.WANT_STRING + Constants.LANOS_NAME);
         }
     }
+    public static class LuxuriousCar {
+         void printlnDesire() {
+            System.out.println(Constants.WANT_STRING + Constants.LUXURIOUS_CAR);
+        }
 
+    }
+    public static class CheapCar {
+        protected void printlnDesire() {
+            System.out.println(Constants.WANT_STRING + Constants.CHEAP_CAR);
+        }
+
+    }
     public static class Constants {
         public static String WANT_STRING = "Я хочу ездить на ";
         public static String LUXURIOUS_CAR = "роскошной машине";
