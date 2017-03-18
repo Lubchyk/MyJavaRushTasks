93409842e8e0099214787c22413d116a59344be0
Lubchyk <lubchyk@yandex.ua>
2017-03-18 18:21:59 +0200

    new tasks

--- Tests/PaternExample.java
+++ Tests/PaternExample.java
@@ -1,7 +1,17 @@
-package PACKAGE_NAME;
-
 /**
  * Created by АНТ on 11.03.2017.
  */
 public class PaternExample {
+
+    /**приклад реалізації шаблона Singleton з лінивою ініціалізацією(lazy initialization)*/
+    public class Sun {
+        private /*static*/ Sun instance;
+
+        private Sun() {
+        }
+        public/*static*/ Sun getInstance() {
+            if (instance == null) instance = new Sun();
+            return instance;
+        }
+    }
 }
