cf955e455cc743af742c25878357ac93087dbae8
Lubchyk <lubchyk@yandex.ua>
2017-04-05 22:30:00 +0300

    add new tasks

--- 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
@@ -5,11 +5,17 @@ package com.javarush.task.task16.task1617;
 */
 
 public class Solution {
-    public static volatile int countSeconds = 3;
+    public static volatile int countSeconds = 4;
+    public static boolean a = true;
 
     public static void main(String[] args) throws InterruptedException {
         RacingClock clock = new RacingClock();
-        //add your code here - добавь код тут
+        if (countSeconds >= 4) {
+            Thread.sleep(3500);
+            clock.interrupt();
+            a = false;
+
+        }
     }
 
     public static class RacingClock extends Thread {
@@ -18,7 +24,17 @@ public class Solution {
         }
 
         public void run() {
-            //add your code here - добавь код тут
+            while (countSeconds != 0) {
+                System.out.print(countSeconds-- + " ");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    System.out.print("Прервано!");
+                }
+
+            }
+            if (a) System.out.print("Марш!");
         }
     }
 }
