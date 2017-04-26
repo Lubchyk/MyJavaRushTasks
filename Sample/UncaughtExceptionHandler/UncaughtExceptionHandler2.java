8cf81d18b1fa236179ec95cb93afd5961b52cf9f
mlv <lubchyk@yandex.ua>
2017-04-26 15:16:43 +0300

    add new task

---
+++ Sample/UncaughtExceptionHandler/UncaughtExceptionHandler2.java
@@ -0,0 +1,72 @@
+package UncaughtExceptionHandler;
+
+/**
+ * Created by mlv on 26.04.2017.
+ * Приклад №2
+ */
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class UncaughtExceptionHandler2
+{
+    public static void main(final String args[]) throws Exception
+    {
+        Thread.UncaughtExceptionHandler handler = new StackWindow("Show Exception Stack", 400, 200);
+        Thread.setDefaultUncaughtExceptionHandler(handler);
+        new Thread() {
+            public void run() {
+                System.out.println(1 / 0);
+            }
+        }.start();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Press Enter for next exception");
+        br.readLine();
+        new Thread() {
+            public void run() {
+                System.out.println(args[0]);
+            }
+        }.start();
+        System.out.print("Press Enter to end");
+        br.readLine();
+        System.exit(0);
+    }
+
+    public static class StackWindow extends JFrame implements Thread.UncaughtExceptionHandler
+    {
+        private JTextArea textArea;
+
+        public StackWindow(String title, final int width, final int height)
+        {
+            super(title);
+            setSize(width, height);
+            textArea = new JTextArea();
+            JScrollPane pane = new JScrollPane(textArea);
+            textArea.setEditable(false);
+            getContentPane().add(pane);
+        }
+
+        public void uncaughtException(Thread t, Throwable e)
+        {
+            addStackInfo(e);
+        }
+
+        public void addStackInfo(final Throwable t)
+        {
+            EventQueue.invokeLater(new Runnable()
+            {
+                public void run() {
+                    // Bring window to foreground
+                    setVisible(true);
+                    toFront();
+                    // Convert stack dump to string
+                    StringWriter sw = new StringWriter();
+                    PrintWriter out = new PrintWriter(sw);
+                    t.printStackTrace(out);
+                    // Add string to end of text area
+                    textArea.append(sw.toString());
+                }
+            });
+        }
+    }
+}
